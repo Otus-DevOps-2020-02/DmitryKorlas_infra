f100361de3ea641c88cf7dec18c2e488ed6caea3
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-03-29 19:30:15 +0300

    cloud-testapp: downgrade mongodb from 4 to 3 because of CI tests fault

--- install-cloud-testapp.sh
+++ install-cloud-testapp.sh
@@ -8,20 +8,37 @@ echo "pwd: $(pwd)"
 sudo apt-get update
 sudo apt-get --assume-yes install ruby-full ruby-bundler build-essential
 
-#install mongodb
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-sudo apt-get --assume-yes install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# install mongodb 4 (failed on CI tests)
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# sudo apt-get --assume-yes install gnupg
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+# install mongodb 3
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get update
 sudo apt-get --assume-yes install mongodb-org
 
+sudo tee /etc/systemd/system/mongodb.service > /dev/null <<EOT
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+
+[Service]
+User=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
 # start mongo
-sudo systemctl start mongod
+sudo systemctl start mongodb
 
 # add mongo to autorun
-sudo systemctl enable mongod
+sudo systemctl enable mongodb
 
 # install reddit demo app
 sudo -i -u appuser bash << EOF
