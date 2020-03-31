f2ebd145832c42958336cb779687aa2ae02482e3
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-03-31 21:49:52 +0300

    packer-base: add packer scripts for ubuntu image creation

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+sudo systemctl start mongod
+
+# add mongo to autorun
+sudo systemctl status mongod
+sudo systemctl enable mongod
