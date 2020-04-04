c1d2d1245f7f5d4357b02d856873b82d5d343964
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-04-04 20:58:37 +0300

    packer-base: remove sudo from install scripts due to it run by packer with sudo privileges

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -e
 
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
 
-sudo systemctl start mongod
+systemctl start mongod
 
 # add mongo to autorun
-sudo systemctl status mongod
-sudo systemctl enable mongod
+systemctl status mongod
+systemctl enable mongod
