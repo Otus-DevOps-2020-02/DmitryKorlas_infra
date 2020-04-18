2774af87c74ea19f0fee76b6880493c42d3bb690
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-04-18 20:22:01 +0300

    terraform-2: move terraform balancer config to another dir

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,7 @@ output "app_external_ip" {
   value = google_compute_instance.app[*].network_interface[0].access_config[0].nat_ip
 }
 
-output "lb_external_ip" {
-  value = google_compute_global_address.external-address.address
-}
+// used with lb.tf
+//output "lb_external_ip" {
+//  value = google_compute_global_address.external-address.address
+//}
