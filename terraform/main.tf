dad73e628b9cad8023e2adf20b881db87d3a3d5d
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-04-18 22:30:10 +0300

    terraform-2: add ip and ssh firewall config

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,9 @@ resource "google_compute_instance" "app" {
 
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+      nat_ip = google_compute_address.app_ip.address
+    }
   }
 
   connection {
@@ -62,4 +64,21 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   # this firewall rule will be applicable for instances with the tags listed in "target_tags"
   target_tags = ["reddit-app"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name        = "default-allow-ssh"
+  description = "TF: Allow SSH from anywhere"
+  network     = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
 }
