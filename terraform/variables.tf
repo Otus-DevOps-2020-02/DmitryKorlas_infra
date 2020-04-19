853a00fc8b4a36b8f41bcf3575147267917c6227
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-04-20 00:12:57 +0300

    terraform-2: add temp bucket using storage-bucket module

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Zone for VM instance"
+  default     = "europe-west1-b"
+}
