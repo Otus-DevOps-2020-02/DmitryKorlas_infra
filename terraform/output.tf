1a7def91ad73a29a31d6d96fd76dffed1c80825f
Dmitry Korlas <dmitry.korlas@gmail.com>
2020-04-19 16:30:55 +0300

    terraform-2: split monolith main.tf config to separate vpc.tf, app.tf, db.tf add 'app' and 'db' images for packer

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,9 @@ output "app_external_ip" {
   value = google_compute_instance.app[*].network_interface[0].access_config[0].nat_ip
 }
 
+output "db_external_ip" {
+  value = google_compute_instance.db[*].network_interface[0].access_config[0].nat_ip
+}
 // used with lb.tf
 //output "lb_external_ip" {
 //  value = google_compute_global_address.external-address.address
